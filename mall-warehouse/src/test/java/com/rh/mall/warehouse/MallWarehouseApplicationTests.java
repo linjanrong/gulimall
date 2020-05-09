c33835047777831333ff08a40e4a170ca642da18
linjanrong <linjanrong221@gmail.com>
2020-05-09 23:36:39 +0800

    first project init

---
+++ mall-warehouse/src/test/java/com/rh/mall/warehouse/MallWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.mall.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
